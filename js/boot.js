4cfa188ce6258e8c550de1e9adcd4ced29921d31
Liam Whan <liam.whan@gmail.com>
2015-07-28 18:14:33 +1000

    Simplify data storage and retrieval, fixed FileManager and data file loader

--- js/boot.js
+++ js/boot.js
@@ -1,9 +1,11 @@
 /**
  * Created by Liam on 27/07/2015.
  */
-var CONFIG = require('config');
-var data = require('filecheck');
-var MVideos = require("videoparser");
+//var CONFIG = require('config');
+//var data = require('filecheck');
+//var MVideos = require("videoparser");
+//
+//
+//global.MVideos = MVideos;
 
-
-global.MVideos = MVideos;
+var FileManager = require('FileManager');
