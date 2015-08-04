dee27ad7ab71ed6f3876463ce73a02fe039841c1
Liam Whan <liam.whan@gmail.com>
2015-08-04 20:12:29 +1000

    Working on build. Concat working, package.json fixed, save added, added banners to js for concat

--- gruntFile.js
+++ gruntFile.js
@@ -1,10 +1,25 @@
 module.exports =function(grunt){
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        buildFolder: 'build',
+        concat: {
+            options: {
+                separator: ';\n'
+            },
+            build: {
+                src: ['node_modules/*.js', "node_modules/angular/angular.js",
+                    "node_modules/angular-route/angular-route.js",
+                    "node_modules/ng-lodash/build/ng-lodash.js",
+                    "node_modules/angular-local-storage/dist/angular-local-storage.js"],
+                dest: '<%= buildFolder %>/node_app.js'
+            }
+
+        },
 
         compress: {
             videos: {
                 options: {
-                    archive: 'build/videos.zip'
+                    archive: 'build/git videos.zip'
                 },
                 mode: 'zip',
                 files: [
@@ -27,10 +42,18 @@ module.exports =function(grunt){
     });
 
 
+    /**
+     * Zip media
+     * Concatenate js and css
+     * minify?
+     * Parse the index file?
+     *
+     */
 
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.registerTask('zipitup', ['compress']);
+    grunt.registerTask('conc', ['concat']);
 
 };
-
